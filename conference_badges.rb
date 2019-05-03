e814a12882ac7ab3300a1bf4be29cb4579f9b5c0
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 04:52:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,5 +25,5 @@ def assign_rooms(attendees)
       attendees.each_with_index do |name, index|
         badges_and_room_assignments.push "Hello, my name is #{name}. My room is number #{index +1}"
       end
-      puts badges_and_room_assignments
+      puts badges_and_room_assignments[0]
   end
