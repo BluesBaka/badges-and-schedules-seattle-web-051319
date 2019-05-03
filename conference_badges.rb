e5743edfd35a766b46cd12550314b1e26fbf3444
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 05:00:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,4 +24,7 @@ def assign_rooms(attendees)
       batch_badge_creator(attendees).each do |badge|
         puts badge
       end
+      assign_rooms(attendees).each do |room|
+        puts room
+      end
   end
