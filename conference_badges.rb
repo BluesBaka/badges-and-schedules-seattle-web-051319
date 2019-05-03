8280dda94d35ac88ec76ae388a8fbe8c2afd26a1
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 04:11:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,8 @@ def badge_maker(name)
 end# Write your code here.
 
 def batch_badge_creator(attendees)
-   attendees.each do |name|
-     return "Hello, my name is #{name}"
+   attendees.collect do |name|
+      "Hello, my name is #{name}"
    end
+   attendees
 end
